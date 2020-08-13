c6ed6612cbeb761fa1808117bf7c0ca8731c5294
clairepeng <zdimple0808@gmail.com>
2020-08-13 23:19:57 +0800

    added toasts

--- social_media/groups/views.py
+++ social_media/groups/views.py
@@ -22,15 +22,12 @@ class CreateGroup(LoginRequiredMixin,generic.CreateView):
 
 class UserGroupList(LoginRequiredMixin,PrefetchRelatedMixin,generic.ListView):
     # groups belonging to an user
-    model = models.Group
     prefetch_related = ('members','posts')
     template_name = 'groups/user_group_list.html'
     context_object_name = 'user_group_list'
 
     def get_queryset(self):
-        return super().get_queryset().filter(
-            members__username__iexact=self.request.user.username
-        )
+        return models.Group.objects.filter(members__username__iexact=self.request.user.username)
     
 class GroupDetail(PrefetchRelatedMixin,generic.DetailView):
     model = models.Group
@@ -45,22 +42,21 @@ class JoinGroup(LoginRequiredMixin,generic.RedirectView):
     
     # Once joining the group, you will be directed to the group detail page
     def get_redirect_url(self, *args, **kwargs):
-        group = get_object_or_404(models.Group,slug=self.kwargs.get('slug'))
-        return reverse('groups:detail',kwargs={'slug':self.kwargs.get('slug')})
+        return reverse('groups:detail',kwargs={'slug':kwargs.get('slug')})
 
     # Make sure the group exists
     def get(self, request, *args, **kwargs):
 
-        group = get_object_or_404(models.Group,slug=self.kwargs.get('slug'))
+        group = get_object_or_404(models.Group,slug=kwargs.get('slug'))
         
         try:
-            models.GroupMembership.objects.create(user=self.request.user,group=group)
+            models.GroupMembership.objects.create(user=request.user,group=group)
         
         except IntegrityError:
-            messages.warning(self.request,'You are already a member!')
+            messages.warning(request,'You are already a member!')
         
         else:
-            messages.success(self.request,'Success')
+            messages.success(request,'Joined the group successfully')
         
         return super().get(request,*args,**kwargs)
 
@@ -68,24 +64,24 @@ class LeaveGroup(LoginRequiredMixin,generic.RedirectView):
     
     # Once joining the group, you will be directed to the group detail page
     def get_redirect_url(self, *args, **kwargs):
-        return reverse('groups:detail',kwargs={'slug':self.kwargs.get('slug')})
+        return reverse('groups:detail',kwargs={'slug':kwargs.get('slug')})
     
     # Make sure the group exists
     def get(self, request, *args, **kwargs):
-
-        group = get_object_or_404(models.Group,slug=self.kwargs.get('slug'))
         
         try:
             membership = models.GroupMembership.objects.filter(
-            user=self.request.user,
-            group=group,
-            ).delete()
+            user=request.user,
+            group=get_object_or_404(
+                models.Group,slug=kwargs.get('slug')),
+            )
         
         except models.GroupMembership.DoesNotExist:
-            messages.warning(self.request,'You aren\'t a group member')
+            messages.warning(request,'You aren\'t a group member')
         
         else:
-            messages.success(self.request,'Left the group!')
+            membership.delete()
+            messages.success(request,'Left the group!')
         
         return super().get(request,*args,**kwargs)
 
